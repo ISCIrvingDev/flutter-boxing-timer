151279a7f1530a93c31d97442a0cb598c389ed3c
Irving Salazar <ISCIrvingDev@gmail.com>
2025-04-25 13:28:11 -0600

    Completar AppTimerService

--- lib/shared/services/timer/timer.repository.dart
+++ lib/shared/services/timer/timer.repository.dart
@@ -2,6 +2,21 @@
 import 'dtos/current_timer.dto.dart';
 
 abstract class IAppTimerRepository {
+  // * Obtiene el Timer actual
   Future<CurrentTimerDto> getCurrentTimer();
-  // Future<CurrentTimerDto> getTimerById(int id);
+
+  // * Obtiene un Timer por medio de su ID
+  Future<CurrentTimerDto> getTimerByKey(String key);
+
+  // * Obtiene una lista de todos los Timers
+  Future<List<String>> getTimers();
+
+  // * Establece un timer como el Timer actual por medio de su Key
+  Future<bool> setCurrentTimer(String key);
+
+  // * Crea un nuevo Timer
+  Future<bool> createTimer(String key, CurrentTimerDto timer);
+
+  // * Elimina un Timer por medio de su Key
+  Future<bool> removeTimer(String key);
 }
--- lib/shared/services/timer/timer.service.dart
+++ lib/shared/services/timer/timer.service.dart
@@ -1,4 +1,6 @@
 // * Repository
+import 'dart:convert';
+
 import 'timer.repository.dart';
 
 // * DTOs
@@ -17,12 +19,64 @@ class AppTimerService implements IAppTimerRepository {
     // data != null CurrentTimerDto.fromJson(data)
 
     // var res = prefs.get('currentTimer') ?? CurrentTimerDto(roundTime: '03:00');
-    var currentTimer = prefs.get('currentTimer');
+    var currentTimer = prefs.getString('currentTimer');
 
     var res =
         currentTimer != null
-            ? currentTimer as CurrentTimerDto
+            ? jsonDecode(currentTimer) as CurrentTimerDto
             : defaultCurrentTimerDto;
+
+    return res;
+  }
+
+  @override
+  Future<CurrentTimerDto> getTimerByKey(String key) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    var timer = prefs.getString(key);
+
+    var res =
+        timer != null
+            ? jsonDecode(timer) as CurrentTimerDto
+            : defaultCurrentTimerDto;
+
+    return res;
+  }
+
+  @override
+  Future<List<String>> getTimers() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    var timers = prefs.getKeys();
+
+    return timers.toList();
+  }
+
+  @override
+  Future<bool> setCurrentTimer(String key) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    var timer = getTimerByKey(key);
+
+    bool res = await prefs.setString('currentTimer', jsonEncode(timer));
+
+    return res;
+  }
+
+  @override
+  Future<bool> createTimer(String key, CurrentTimerDto timer) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    var res = await prefs.setString(key, jsonEncode(timer));
+
+    return res;
+  }
+
+  @override
+  Future<bool> removeTimer(String key) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    bool res = await prefs.remove(key);
 
     return res;
   }
